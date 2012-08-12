26c4b914888e8bb7ceaab80ee645b514797409d8
Anant Bhardwaj <anantb@cs.stanford.edu>
2012-08-12 09:57:20 -0700

    final?

--- trishaatt/trishaatt/settings.py
+++ trishaatt/trishaatt/settings.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gunicorn',
     'trishaatt'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
