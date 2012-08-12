ec667d86387bb4c99acaa84c561df4659814a227
Anant Bhardwaj <anantb@cs.stanford.edu>
2012-08-12 00:02:51 -0700

    model changes

--- trishaatt/trishaatt/models.py
+++ trishaatt/trishaatt/models.py
@@ -10,3 +10,29 @@ class User(models.Model):
 
 	def __unicode__(self):
 		return self.name
+
+
+class Project(models.Model):
+	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length=100)
+	desc = models.CharField(max_length=300)
+	filename = models.CharField(max_length=100)
+	deadline = models.TimeField()
+	owner = models.ForeignKey('User')
+
+
+	def __unicode__(self):
+		return self.name
+		
+		
+class Task(models.Model):
+	id = models.AutoField(primary_key=True)
+	desc = models.CharField(max_length=100)
+	project_id = models.ForeignKey('Project')
+	owner = models.ForeignKey('User')
+	step_id = models.IntegerField()
+	step_deadline = models.TimeField()
+
+
+	def __unicode__(self):
+		return self.name
